4cf2c4aa8f3bbe31d502866e0a2f079a1585bb56
oconnbra25 <123963813+oconnbra25@users.noreply.github.com>
2025-02-07 10:56:23 -0500

    Create Partay.java

---
+++ Partay.java
@@ -0,0 +1,16 @@
+/**
+* Partay.java class for running the party
+* @author Brady OC
+* @since 2/7/25
+* Precodnitions: attendee objects to put in at table
+* Postconditions: generates attendees seat numbers
+* 
+*/
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList; // Import the ArrayList utilities
+
+public class Partay {
+	public Partay (
