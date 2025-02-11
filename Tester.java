c0fa88a4d331e5b4beb587505cd13d0f2ebdef3b
oconnbra25 <123963813+oconnbra25@users.noreply.github.com>
2025-02-11 09:36:43 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 /**
 * Tester.java class for testing the program
 * @author Brady OC
-* @since 2/7/25
+* @since 2/11/25
 * Precodnitions: nothing
 * Postconditions: runs the entire program from start to end
 * 
@@ -14,31 +14,21 @@ import java.util.ArrayList; // Import the ArrayList utilities
 
 public class Tester {
 	public static void main(String[] args) {
-		//create array list
 		
-		ArrayList<Attendee> attendees = new ArrayList<Attendee>(); //list for the attendees
+		//scanner to search for inputs by the user
+		Scanner scan1 = new Scanner(System.in);
 		
-		//this try catch is reading the file 
-		Scanner scan = new Scanner(System.in); //for keyboard inputs 
-		try {
-			File myObj = new File("partyguests.txt");
-			Scanner myReader = new Scanner(myObj); //for the file inputs
-			while (myReader.hasNextLine()) {
-				String data = myReader.nextLine();
-				String[] eachColumn = data.split(",");
-				Attendee att = new Attendee(eachColumn[2] + " " + eachColumn[1], Integer.parseInt(eachColumn[3]));
-				attendees.add(att);
-				//System.out.println(data);
-		}
-		myReader.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
-			e.printStackTrace();
-		}
+		//prompting user for # of tables & chairs
+		System.out.println("Enter how many tables and chairs you want like so: (Tables),(Chairs)");
+		String tableChairTemp = scan1.nextLine();
+		String[] tableChair = tableChairTemp.split(",");
+		
+		//making a partay object
+		Partay party = new Partay(Integer.parseInt(tableChair[0]), Integer.parseInt(tableChair[1]));
 		
 		//attempting the loop to run the code
 		Boolean runner = true; //to make sure the script is running
-		Scanner scan1 = new Scanner(System.in); //scanner to search for inputs by the user
+		
 		while (runner)
 		{
 			System.out.println("Enter the number corresponding to what you would like to do:");
@@ -53,9 +43,7 @@ public class Tester {
 				//attempting to add someone to the array list
 				System.out.println("Please enter their first and last name and company ID like so: (first name) (last name),(companyID)");
 				String registerPerTemp = scan1.nextLine();
-				String[] registerPer = registerPerTemp.split(",");
-				Attendee att = new Attendee(registerPer[0], Integer.parseInt(registerPer[1]));
-				attendees.add(att);
+				party.addAttendee(registerPerTemp);
 				
 				System.out.println("\n");
 			}
@@ -65,19 +53,7 @@ public class Tester {
 				System.out.println("Please enter their first and last name like so: (first name) (last name)");
 				String searchAttendee = scan1.nextLine();
 				
-				Boolean tempFinder = true;
-				for (Attendee attendee : attendees)
-				{
-					if (attendee.getName().equals(searchAttendee))
-					{
-						System.out.println(attendee.getAttendee());
-						tempFinder = false;
-					}
-				}
-				if (tempFinder)
-				{
-					System.out.println("Person not found");
-				}
+				System.out.println(party.searchAttendees(searchAttendee));
 				
 				System.out.println("\n");
 			}
@@ -86,11 +62,6 @@ public class Tester {
 				runner = false;
 			}
 		}
-		
-		
-		
-		//add an else after this if later to maybe re prompt the user to put in what they want to do
-		
 		
 		//Graveyard:
 			/* //spacer between the data being printed and the test print for the entire array list
